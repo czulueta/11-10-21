a944960def937dd245ca4667f2a5d813df8987de
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-11-11 07:01:40 -0800

    built complete backend with 2 routes 2 models a server and built frontend with 8 components 1 context provider but not complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react"
+import { Switch, Route } from "react-router-dom"
+import Navbar from "./components/Navbar.js"
+import Auth from "./components/Auth.js"
+import Profile from "./components/Profile.js"
+import Public from "./components/Public.js"
+import { UserContext } from "../context/UserProvider.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const { token, logout } = useContext(UserContext)
+  return(
+    <div>
+      <Navbar logout={logout}/>
+      <Switch>
+        <Route
+          path="/"
+          render={() => token ? <Redirect path="/profile"/> : <Auth />} />
+        <Route
+          path="/"
+          render={() => <Profile />} />
+        <Route
+          path="/"
+          render={() => <Public />} />
+      </Switch>
     </div>
-  );
+  )
 }
-
-export default App;
